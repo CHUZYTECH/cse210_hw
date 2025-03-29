85260acc1a76d76f9036b3ebb7d73bd61412c469
Oko Samuel Ogbonnia <139375523+CHUZYTECH@users.noreply.github.com>
2025-03-29 09:59:38 +0100

    Update Program.cs

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,55 @@
 using System;
 
+// Abstract base class for Video
+abstract class Video
+{
+    public abstract void Play();
+    public abstract void Like();
+    public abstract string GetDetails();
+}
+
+// Concrete class implementing Video
+class YouTubeVideo : Video
+{
+    private string title;
+    private int duration;
+    private int views;
+    private int likes;
+
+    public YouTubeVideo(string title, int duration)
+    {
+        this.title = title;
+        this.duration = duration;
+        this.views = 0;
+        this.likes = 0;
+    }
+
+    public override void Play()
+    {
+        views++;
+        Console.WriteLine($"Playing '{title}' ({duration} mins). Views: {views}");
+    }
+
+    public override void Like()
+    {
+        likes++;
+        Console.WriteLine($"Liked '{title}'. Total Likes: {likes}");
+    }
+
+    public override string GetDetails()
+    {
+        return $"Title: {title}, Duration: {duration} mins, Views: {views}, Likes: {likes}";
+    }
+}
+
+// Main Program
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        YouTubeVideo video1 = new YouTubeVideo("Introduction to OOP", 10);
+        video1.Play();
+        video1.Like();
+        Console.WriteLine(video1.GetDetails());
     }
 }
