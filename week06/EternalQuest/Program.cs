892802f18d521012ea491f0f65adb1d8556e8f11
Oko Samuel Ogbonnia <139375523+CHUZYTECH@users.noreply.github.com>
2025-04-14 17:06:15 +0100

    Update Program.cs

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager manager = new GoalManager();
+        bool quit = false;
+
+        while (!quit)
+        {
+            Console.WriteLine("\nMenu Options:");
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6. Display Score");
+            Console.WriteLine("7. Quit");
+
+            Console.Write("Select an option: ");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    manager.CreateGoal();
+                    break;
+                case "2":
+                    manager.ListGoals();
+                    break;
+                case "3":
+                    manager.SaveGoals();
+                    break;
+                case "4":
+                    manager.LoadGoals();
+                    break;
+                case "5":
+                    manager.RecordEvent();
+                    break;
+                case "6":
+                    manager.DisplayScore();
+                    break;
+                case "7":
+                    quit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option!");
+                    break;
+            }
+        }
     }
 }
