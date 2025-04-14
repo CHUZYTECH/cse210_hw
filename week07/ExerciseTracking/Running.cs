b1eb9fa28019f8411fec8e1da130251fb1ea1e24
Oko Samuel Ogbonnia <139375523+CHUZYTECH@users.noreply.github.com>
2025-04-14 17:11:35 +0100

    Add files via upload

---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Running : Activity
+{
+    private double _distanceKm;
+
+    public Running(DateTime date, int lengthMinutes, double distanceKm)
+        : base(date, lengthMinutes)
+    {
+        _distanceKm = distanceKm;
+    }
+
+    public override double GetDistance() => _distanceKm;
+
+    public override double GetSpeed() => (_distanceKm / LengthMinutes) * 60;
+
+    public override double GetPace() => LengthMinutes / _distanceKm;
+} 
+
